7702a02e6b35ddf13de858c38c500a5fd9d90edb
justice jennings <justice.l.jennings@gmail.com>
2023-11-16 16:11:51 -0500

    fetched poke napi data in app jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import PokeApi from './components/pokeAPI';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokeData, setPokeData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${PokeApi}api/v2/pokemon`);
+        const data = await response.json();
+        setPokeData(data);
+        console.log(data.results);
+      } catch (error) {
+        console.error('Error fetching poke data:', error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
     <>
---
+++ src/components/pokeAPI.jsx
@@ -0,0 +1,7 @@
+
+// store API in Variable
+const PokeApi = `https://pokeapi.co/`;
+
+
+
+export default PokeApi;
